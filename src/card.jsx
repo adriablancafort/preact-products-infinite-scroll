85ab8c8c8379670ec948b068fd7fabfd4b4bc694
Adria Blancafort <adriablancafort@gmail.com>
2024-03-16 01:04:06 +0100

    initial commit

---
+++ src/card.jsx
@@ -0,0 +1,14 @@
+function Card({ product }) {
+    return (
+        <div class="card">
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>Price: {product.price}</p>
+            <p>Category: {product.category}</p>
+            <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/products.jsx
@@ -0,0 +1,47 @@
+import { render } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import Card from './card';
+import './index.css';
+
+function Products() {
+    
+    const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const loadProducts = () => {
+        fetch(`https://fakestoreapi.com/products?page=${page}`)
+            .then(res => res.json())
+            .then(newProducts => setProducts(prevProducts => [...prevProducts, ...newProducts]));
+    }
+
+    const handleScroll = () => {
+        const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight || window.innerHeight;
+        const scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+        if (scrolledToBottom) {
+            setPage(prevPage => prevPage + 1);
+        }
+    }
+
+    useEffect(() => {
+        loadProducts();
+    }, [page]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <>
+            <h1 class="title">Products</h1>
+            <div class="grid" >
+                {products.map(product => <Card key={product.id} product={product} />)}
+            </div>
+        </>
+    );
+}
+
+render(<Products />, document.getElementById('root'));
